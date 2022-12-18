e5a9d3b5e2a3810e7a1f64d4e01424b3c078b07b
minho <minhokim0806@gmail.com>
2022-12-19 00:25:15 +0900

    [feature] Security Config 변경
    CSRF 관련 주석 추가. 인증 거치지 않는 api 설정. 해당 api들은 명세서에 맞춰 변경할 예정

--- src/main/java/com/example/haelogproject/common/security/WebSecurityConfig.java
+++ src/main/java/com/example/haelogproject/common/security/WebSecurityConfig.java
@@ -1,9 +1,14 @@
 package com.example.haelogproject.common.security;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,10 +16,19 @@ import org.springframework.security.web.SecurityFilterChain;
 public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
         // CSRF 설정
+        // 공격자가 인증된 브라우저에 저장된 쿠키의 세션 정보를 활용하여 웹 서버에 사용자가 의도하지 않은 요청을 전달하는 것
         http.csrf().disable();
 
-        http.authorizeRequests().anyRequest().authenticated();
+        // Session 방식 비활성화
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.authorizeRequests().
+                // SignUp, Login api는 인증 없이도 승인
+                antMatchers("/signup").permitAll().
+                antMatchers("/login").permitAll().
+                anyRequest().authenticated();
 
         // 로그인 사용
         http.formLogin();
