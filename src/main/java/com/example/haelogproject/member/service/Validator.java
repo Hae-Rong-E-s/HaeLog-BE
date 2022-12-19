19b7cde6f712f9da5a205ba268638596c897fb5b
hjun0917 <hjun0917@naver.com>
2022-12-20 02:42:33 +0900

    [comment] 고민해볼 사항에 대해 주석처리

--- src/main/java/com/example/haelogproject/member/service/Validator.java
+++ src/main/java/com/example/haelogproject/member/service/Validator.java
@@ -21,6 +21,9 @@ public class Validator {
     }
 
     public boolean validNickname(String nickname) {
+        // TODO
+        //  닉네임 조건을 바꾸는게 좋지 않을까?
+
         // "닉네임은 알파벳 대소문자와 숫자로 구성된 2~8자리로 작성해주세요."
         final String regex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{2,8}$";
 
