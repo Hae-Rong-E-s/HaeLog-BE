71e11f1d6cf8f3caab75780d90ea01f81a575491
Hyun <itsmine096@gmail.com>
2022-12-20 15:34:37 +0900

    [feat] 게시물 삭제기능 추가

--- src/main/java/com/example/haelogproject/post/repository/PostTagRepository.java
+++ src/main/java/com/example/haelogproject/post/repository/PostTagRepository.java
@@ -6,4 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface PostTagRepository extends JpaRepository<PostTag, Long> {
 
     void deleteAllByPost_Id(Long postId);
+
+    boolean existsByTag_Id(Long tagId);
 }
--- src/main/java/com/example/haelogproject/post/service/PostService.java
+++ src/main/java/com/example/haelogproject/post/service/PostService.java
@@ -1,5 +1,6 @@
 package com.example.haelogproject.post.service;
 
+import com.example.haelogproject.comment.repository.CommentRepository;
 import com.example.haelogproject.member.entity.Member;
 import com.example.haelogproject.post.dto.PostRequestDto;
 import com.example.haelogproject.post.entity.Post;
@@ -13,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 
@@ -23,6 +25,7 @@ public class PostService {
     private final PostRepository postRepository;
     private final TagRepository tagRepository;
     private final PostTagRepository postTagRepository;
+    private final CommentRepository commentRepository;
     private final PostMapper mapper;
 
     @Transactional
@@ -62,6 +65,16 @@ public class PostService {
     @Transactional
     public void updatePost(Long postId, PostRequestDto request, Member member) {
 
+        // 기존에 저장된 게시물 불러오기
+        Post post = postRepository.findById(postId).orElseThrow(
+                ()-> new IllegalArgumentException("게시글이 존재하지 않습니다.")
+        );
+
+        // 게시물의 작성자가 요청한 사용자와 일치하는지 확인
+        if (!post.getMemberId().equals(member.getMemberId())) {
+            throw new IllegalArgumentException("게시글 수정 권한이 없습니다.");
+        }
+
         // 본문 내용 미리보기를 저장 할 필드
         String summary;
         // 내용 미리보기 저장을 위해 150자 이상일 경우 subString으로 내용을 잘라준다.
@@ -70,11 +83,6 @@ public class PostService {
         } else {
             summary = request.getContent().substring(0, 150);
         }
-
-        // 기존에 저장된 게시물 불러오기
-        Post post = postRepository.findById(postId).orElseThrow(
-                ()-> new IllegalArgumentException("게시글이 존재하지 않습니다.")
-        );
 
         // 기존에 해당 포스트에 저장된 태그를 전부 삭제하고 새로 생성
         postTagRepository.deleteAllByPost_Id(post.getPostId());
@@ -98,5 +106,34 @@ public class PostService {
 
         // 게시물 내용 변경
         post.update(request.getTitle(), request.getContent(), summary);
+    }
+
+    public void deletePost(Long postId, Member member) {
+        // 기존에 저장된 게시물 불러오기
+        Post post = postRepository.findById(postId).orElseThrow(
+                ()-> new IllegalArgumentException("게시글이 존재하지 않습니다.")
+        );
+
+        // 게시물의 작성자가 요청한 사용자와 일치하는지 확인 일치하지 않을 경우 예외 발생
+        if (!post.getMemberId().equals(member.getMemberId())) {
+            throw new IllegalArgumentException("게시글 수정 권한이 없습니다.");
+        }
+
+        // 태그(PostTag) 삭제
+        postTagRepository.deleteAllByPost_Id(postId);
+
+        // 태그 이름(Tag)을 사용하는 게시물이 없을 경우 태그도 삭제
+        List<Tag> tags = tagRepository.findAllByMember_Id(member.getMemberId());
+        for (Tag tag : tags) {
+            if (!postTagRepository.existsByTag_Id(tag.getTagId())) {
+                tagRepository.delete(tag);
+            }
+        }
+
+        // 댓글 삭제
+        commentRepository.deleteAllByPost_Id(postId);
+
+        // 게시물 삭제
+        postRepository.deleteById(postId);
     }
 }
