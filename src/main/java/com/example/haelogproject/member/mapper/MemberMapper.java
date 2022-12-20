787e95aa1341fd9fb1e38b7177ce298af4f5fb7d
hjun0917 <hjun0917@naver.com>
2022-12-20 14:32:35 +0900

    [refactor] Mapper 가 Builder 를 사용하도록 코드 변경

--- src/main/java/com/example/haelogproject/member/mapper/MemberMapper.java
+++ src/main/java/com/example/haelogproject/member/mapper/MemberMapper.java
@@ -7,9 +7,12 @@ import org.springframework.stereotype.Component;
 @Component
 public class MemberMapper {
     public Member toMember(RequestUserSignup requestUserSignup) {
-        return new Member(requestUserSignup.getLoginId(),
-                requestUserSignup.getPassword(),
-                requestUserSignup.getNickname(),
-                requestUserSignup.getDescription());
+        //  엔티티의 @Builder 를 통해 사용되어짐.
+        return Member.builder()
+                .loginId(requestUserSignup.getLoginId())
+                .nickname(requestUserSignup.getNickname())
+                .description(requestUserSignup.getDescription())
+                .password(requestUserSignup.getPassword())
+                .build();
     }
 }
